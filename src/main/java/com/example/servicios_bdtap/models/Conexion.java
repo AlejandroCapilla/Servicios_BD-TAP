19a19c4ab9598a8315e2cb54d5aa47724060fe1a
AlejandroCapilla <19031028@itcelaya.edu.mx>
2021-12-02 21:12:55 -0600

    fin hoy

--- src/main/java/com/example/servicios_bdtap/models/Conexion.java
+++ src/main/java/com/example/servicios_bdtap/models/Conexion.java
@@ -1,22 +1,5 @@
 package com.example.servicios_bdtap.models;
 
 public class Conexion {
-    private static String server = "127.0.0.1";
-    private static String user = "root";
-    private static String pwd = "1234";
-    private static String db = "Servicios";
 
-    /*
-    public static Connection conexion;
-
-    public static void getConexion() {
-        try {
-            Class.forName("org.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://"+server+":3306/"+db, user,pwd);
-
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-    */
 }
